e5c4accc8e5972267c3f8196574e1bf912e2af34
payam <payam.4uzandeh@gmail.com>
2022-10-06 22:22:23 -0700

    Completed the implementation and dev testing of Dataset Model, replaced folder test code with Payam's C0 code for troubleshooting

--- src/model/Dataset.ts
+++ src/model/Dataset.ts
@@ -1,7 +1,7 @@
-import {ICourse, Course} from "./Course";
-import {InsightDatasetKind, InsightError} from "../controller/IInsightFacade";
+import {Course, ICourse} from "./Course";
+import {InsightDatasetKind} from "../controller/IInsightFacade";
 import JSZip, {JSZipObject} from "jszip";
-import {fdatasync} from "fs";
+import {isValidId} from "./Utility";
 
 export interface IDataset {
 	id: string,
@@ -10,42 +10,69 @@ export interface IDataset {
 }
 
 export class Dataset {
-	public static async parseDataset(id: string, content: string, kind: InsightDatasetKind): Promise<Dataset> {
+	/**
+	 * @REQUIRES
+	 * @EFFECT
+	 */
+	public static parseDataset(id: string, content: string, kind: InsightDatasetKind): Promise<Dataset> {
+		const dataset: IDataset = {id: "", kind: InsightDatasetKind.Sections, courses: []};
+		let skippedCoursesCount = 0;
+		let promises: Array<Promise<any>> = [];
 
-		const dataset: IDataset = {} as IDataset;
-		const zip = new JSZip();
-		await zip.loadAsync(content, {base64: true});
-		let countSkip: number;
-		const folders = zip.filter((pathName: string, file: JSZip.JSZipObject) => {
-			return (file.name.startsWith("courses/"));
-		});
-		if (folders.length === 0) {
-			// we are not having the courses root folder
-			return Promise.reject(new InsightError());
+		if (!isValidId(id)) {
+			return Promise.reject("Invalid id");
+		} else if (!content) {
+			return Promise.reject("Empty content");
 		}
-		// Loop through each file and pass them to parseCourse
-		zip.folder("courses")?.forEach((pathName: string, file: JSZip.JSZipObject) => {
-			// console.log(pathName);
-			return Course.parseCourse(file)
-				.then((course) => {
-					// else, populate the dataset.courses variable
-					dataset.courses.push(course);
-				})
-				.catch(() => {
-					// If the parseCourse gave an error, don't throw it, just skip it
-					countSkip++;
-					console.log("Number of files skipped: " + countSkip);
+		return getContent(content)
+			.then(async function(folder) {
+				folder.forEach((pathName: string, file: JSZipObject) => {
+					promises.push(Course.parseCourse(file)
+						.then((course) => {
+							// populating the dataset.courses variable
+							dataset.courses.push(course);
+						}).catch((err) => {
+							console.log("Invalid Course: " + err);
+							// skipping
+							skippedCoursesCount++;
+						})
+					);
 				});
-		});
-		// Check if the dataset.courses is empty => throw an error
-		if (dataset.courses.length === 0) {
-			return Promise.reject(new InsightError());
-		}
-		dataset.courses.forEach((course) => {
-			console.log(course);
-		});
-		dataset.id = id;
-		dataset.kind = kind;
-		return dataset;
+				await Promise.all(promises);
+				console.log(`Skipped ${skippedCoursesCount} sections`);
+
+				// Checking if the dataset.courses is empty
+				if (dataset.courses.length === 0) {
+					return Promise.reject("No valid sections in the dataset");
+				}
+				dataset.id = id;
+				dataset.kind = kind;
+				return dataset;
+			}).catch((err) => {
+				return Promise.reject(err);
+			});
 	}
+}
+
+// ---------- helper functions -----------
+function getContent(content: string) {
+	const zip = new JSZip();
+	return zip.loadAsync(content, {base64: true})
+		.then(() => {
+			const folders = zip.filter((pathName: string, file: JSZip.JSZipObject) => {
+				return (file.name.startsWith("courses/"));
+			});
+			if (folders.length === 0) {
+				// we are not having the courses root folder
+				return Promise.reject("Zip file is missing the 'courses' folder");
+			}
+			const folder = zip.folder("courses");
+			if (!folder) {
+				return Promise.reject("Zip filed is missing the 'courses' folder");
+			} else {
+				return folder;
+			}
+		}).catch((err) => {
+			return Promise.reject(err);
+		});
 }
--- test/DatasetTest.ts
+++ test/DatasetTest.ts
@@ -1,4 +1,4 @@
-import {IDataset, Dataset} from "../src/model/Dataset";
+import {Dataset} from "../src/model/Dataset";
 
 import * as fs from "fs-extra";
 import chai, {expect} from "chai";
@@ -6,115 +6,135 @@ import chaiAsPromised from "chai-as-promised";
 import {InsightDatasetKind, InsightError} from "../src/controller/IInsightFacade";
 import {ISection, Section} from "../src/model/Section";
 import {readJSON} from "fs-extra";
+import {Course} from "../src/model/Course";
+import JSZip from "jszip";
 
 
 chai.use(chaiAsPromised);
 
-describe("Model testing", function () {
-	let dataset: Dataset;
 
-	const persistDirectory = "./data";
-	const datasetContents = new Map<string, string>();
+let dataset: Dataset;
 
-	const datasetsToLoad: {[key: string]: string} = {
-		sections: "./test/resources/archives/pair.zip",
-		invalidDir: "./test/resources/archives/rtdirwrong.zip",
-		notInJsonfile: "./test/resources/archives/notinjson.zip",
-		noValidSec: "./test/resources/archives/novalidsec.zip",
-		skipFiles: "./test/resources/archives/skipfiles.zip",
-		rtdirwrong: "./test/resources/archives/rtdirwrong.zip",
-		validquerytest: "./test/resources/archives/validquerytest.zip",
-		valid_small: "./test/resources/archives/courses-valid-small.zip",
-		valid_singleSection: "./test/resources/archives/courses-valid-singleSection.zip",
-		invalid_course: "./test/resources/archives/courses-invalid-course.zip",
-		invalid_empty: "./test/resources/archives/courses-invalid-empty.zip",
-		invalid_name: "./test/resources/archives/courses-invalid-name.zip",
-		invalid_section: "./test/resources/archives/courses-invalid-section.zip",
-		invalid_structure: "./test/resources/archives/courses-invalid-structure.zip",
-	};
+const persistDirectory = "./data";
+const datasetContents = new Map<string, string>();
 
-	before(function () {
-		console.info("\n-----------------------------");
+const datasetsToLoad: {[key: string]: string} = {
+	sections: "./test/resources/archives/pair.zip",
+	invalidDir: "./test/resources/archives/rtdirwrong.zip",
+	notInJsonfile: "./test/resources/archives/notinjson.zip",
+	noValidSec: "./test/resources/archives/novalidsec.zip",
+	skipFiles: "./test/resources/archives/skipfiles.zip",
+	rtdirwrong: "./test/resources/archives/rtdirwrong.zip",
+	validquerytest: "./test/resources/archives/validquerytest.zip",
+	valid_small: "./test/resources/archives/courses-valid-small.zip",
+	valid_singleSection: "./test/resources/archives/courses-valid-singleSection.zip",
+	invalid_course: "./test/resources/archives/courses-invalid-course.zip",
+	invalid_empty: "./test/resources/archives/courses-invalid-empty.zip",
+	invalid_name: "./test/resources/archives/courses-invalid-name.zip",
+	invalid_section: "./test/resources/archives/courses-invalid-section.zip",
+	invalid_structure: "./test/resources/archives/courses-invalid-structure.zip",
+};
 
-		// This section runs once and loads all datasets specified in the datasetsToLoad object
-		for (const key of Object.keys(datasetsToLoad)) {
-			const content = fs.readFileSync(datasetsToLoad[key]).toString("base64");
-			datasetContents.set(key, content);
+before(function () {
+	console.info("\n-----------------------------");
+
+	// This section runs once and loads all datasets specified in the datasetsToLoad object
+	for (const key of Object.keys(datasetsToLoad)) {
+		const content = fs.readFileSync(datasetsToLoad[key]).toString("base64");
+		datasetContents.set(key, content);
+	}
+	// Just in case there is anything hanging around from a previous run of the test suite
+	fs.removeSync(persistDirectory);
+});
+
+
+after(function () {
+	console.info("\n-----------------------------");
+});
+
+// beforeEach(function () {
+// });
+
+afterEach(function () {
+	fs.removeSync(persistDirectory);
+});
+
+it("Should parse the Dataset : Small", async function () {
+	dataset = await Dataset.parseDataset(
+		"ubc", datasetContents.get("validquerytest") ?? "", InsightDatasetKind.Sections
+	)
+		.catch((err) => {
+			expect.fail("Should have not thrown an error");
+		});
+});
+
+it("Should parse the Dataset : Large", async function () {
+	dataset = await Dataset.parseDataset(
+		"ubc", datasetContents.get("sections") ?? "", InsightDatasetKind.Sections
+	);
+});
+
+it("Should not parse the Dataset : Invalid syntax", function () {
+	const err = Dataset.parseDataset(
+		"ubc", datasetContents.get("invalid_course") ?? "", InsightDatasetKind.Sections
+	);
+	return expect(err).eventually.to.be.rejected;
+});
+
+it("Should not parse the Dataset : Invalid file", function () {
+	const err = Dataset.parseDataset(
+		"ubc", datasetContents.get("notInJsonfile") ?? "", InsightDatasetKind.Sections
+	);
+	return expect(err).eventually.to.be.rejected;
+});
+
+it("Should reject with InsightError, not start with courses", function () {
+	// const dataset1 : Promise<Dataset>;
+	const err = Dataset.parseDataset(
+		"ubc", datasetContents.get("rtdirwrong") ?? "", InsightDatasetKind.Sections
+	);
+	return expect(err).eventually.to.be.rejected;
+
+});
+
+it("Should return true, json file contains title", function () {
+	Section.parseSection(
+		{
+			Year: "2008",
+			id: 24495,
+			Subject: "astr",
+			Title: "adv plnt comm",
+			Professor: "",
+			Audit: 1,
+			Course: "526",
+			Pass: 6,
+			Fail: 0,
+			Avg: 83.83,
+			// Section: "overall"
 		}
-		// Just in case there is anything hanging around from a previous run of the test suite
-		fs.removeSync(persistDirectory);
+	).catch((err) => {
+		console.log(err);
 	});
+});
 
-	describe("Dataset class", function () {
-		after(function () {
-			console.info("\n-----------------------------");
-		});
+it("Course.parse - valid data -> should parse the course", async function () {
+	const content = fs.readFileSync("./test/resources/files/valid");
+	const myFile = new JSZip().file("./test/resources/files/valid", content)
+		.file("./test/resources/files/valid");
+	if (myFile === null) {
+		expect.fail("Bad test, file is invalid");
+	}
+	const course = await Course.parseCourse(myFile);
+	return expect(course.sections).to.have.length(39);
+});
 
-		// beforeEach(function () {
-		// });
-
-		afterEach(function () {
-			fs.removeSync(persistDirectory);
-		});
-
-		it("Should parse the Dataset : Small", async function () {
-			dataset = await Dataset.parseDataset(
-				"ubc", datasetContents.get("validquerytest") ?? "", InsightDatasetKind.Sections
-			)
-				.catch((err) => {
-					expect.fail("Should have not thrown an error");
-				});
-		});
-
-		it("Should parse the Dataset : Large", async function () {
-			dataset = await Dataset.parseDataset(
-				"ubc", datasetContents.get("sections") ?? "", InsightDatasetKind.Sections
-			);
-		});
-
-		it("Should not parse the Dataset : Invalid syntax", function () {
-			const err = Dataset.parseDataset(
-				"ubc", datasetContents.get("invalid_course") ?? "", InsightDatasetKind.Sections
-			);
-			return expect(err).eventually.to.be.rejectedWith(InsightError);
-		});
-
-		it("Should not parse the Dataset : Invalid file", function () {
-			const err = Dataset.parseDataset(
-				"ubc", datasetContents.get("notInJsonfile") ?? "", InsightDatasetKind.Sections
-			);
-			return expect(err).eventually.to.be.rejectedWith(InsightError);
-		});
-
-		it("Should reject with InsightError, not start with courses", function () {
-			// const dataset1 : Promise<Dataset>;
-			const err = Dataset.parseDataset(
-				"ubc", datasetContents.get("rtdirwrong") ?? "", InsightDatasetKind.Sections
-			);
-			return expect(err).eventually.to.be.rejectedWith(InsightError);
-			// const result = parseDataset(
-			// 	"ubc", datasetContents.get("rtdirwrong") ?? "", InsightDatasetKind.Sections
-			// );
-			// return expect(result).eventually.to.be.rejectedWith(InsightError);
-
-		});
-
-		it("Should return true, json file contains title", function () {
-			Section.parseSection(null
-			// 	{
-			// 	Title: "adv plnt comm",
-			// 	id: 24495,
-			// 	Professor: "",
-			// 	Audit: 1,
-			// 	Year: "2008",
-			// 	Course: "526",
-			// 	Pass: 6,
-			// 	Fail: 0,
-			// 	Avg: 83.83
-			// }
-			).catch(() => {
-				console.log("Patata");
-			});
-		});
-	});
+it("Course.parse - invalid data -> should throw error", function () {
+	const content = fs.readFileSync("./test/resources/files/invalidSyntax");
+	const myFile = new JSZip().file("./test/resources/files/invalidSyntax", content)
+		.file("./test/resources/files/invalidSyntax");
+	if (myFile === null) {
+		expect.fail("Bad test, file is invalid");
+	}
+	const course = Course.parseCourse(myFile);
+	return expect(course).eventually.to.be.rejected;
 });
