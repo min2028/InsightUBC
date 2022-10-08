4808c0bf2bf86e1cb4009770533833eb993d8dcc
min2028 <ronaldaungkhantmin@gmail.com>
2022-10-07 17:49:13 -0700

    added addDataset functions

--- src/model/Utility.ts
+++ src/model/Utility.ts
@@ -1,3 +1,9 @@
+import * as fs from "fs-extra";
+import {persistDirectory} from "../controller/InsightFacade";
+import path from "path";
+
+const idsPath = "/_IDLIST.json";
+
 export function isValidId(id: string, idList?: string[]) {
 	if (/(_+) | (^\s*$)/.test(id)) {
 		return false;
@@ -6,3 +12,33 @@ export function isValidId(id: string, idList?: string[]) {
 	}
 	return true;
 }
+
+export function readDatasetIDs(): string[] {
+	if (!fs.existsSync(persistDirectory + idsPath)) {
+		return [];
+	}
+	const idsFileContent = fs.readJSONSync(persistDirectory + idsPath);
+	return idsFileContent ? idsFileContent.IDs : [];
+}
+
+
+// export function createDirAndWriteIDs(id: string): void {
+// 	let prevIds: string[] = [];
+// 	if (!fs.existsSync(persistDirectory)) {
+// 		fs.mkdirSync(path.join(persistDirectory + idsPath), {recursive: true});
+// 	} else {
+// 		prevIds = fs.readJSONSync(persistDirectory + idsPath).IDs as string[];
+// 	}
+// 	prevIds.push(id);
+// 	fs.writeJSONSync(persistDirectory + idsPath, prevIds);
+// }
+
+export function removeFromDatasetIDs(id: string) {
+	let IDs = readDatasetIDs();
+	if (!IDs.length || !IDs.includes(id)) {
+		return IDs;
+	}
+	IDs.splice(IDs.indexOf(id), 1);
+	return;
+}
+
