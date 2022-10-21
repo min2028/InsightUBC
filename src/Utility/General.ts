114c1ae022f312eec28428cf3e7eda37403d58e9
payam <payam.4uzandeh@gmail.com>
2022-10-20 20:00:36 -0700

    Fixed every bugs #3 and added extra tests

--- src/Utility/General.ts
+++ src/Utility/General.ts
@@ -1,8 +1,2 @@
-export function isValidId(id: string, idList?: string[]): boolean {
-	if (/(_+)|(^\s*$)/.test(id)) {
-		return false;
-	} else if (idList) {
-		return !idList.includes(id);
-	}
-	return true;
-}
+export const isValidId = ((id: string): boolean => !/(_+)|(^\s*$)/.test(id));
+export const isIdInList = ((id: string, idList: string[]) => idList.includes(id));
