4c7fbf4b16ff48d90e32305d2fff3c03b52e0316
min2028 <ronaldaungkhantmin@gmail.com>
2022-11-30 18:11:10 -0800

    Finished the design of ui

--- frontend/public/frontend.js
+++ frontend/public/frontend.js
@@ -8,15 +8,16 @@ const coursesInfo_table = document.getElementById("coursesTable");
 // Room fields and variables
 const roomName_textInput = document.getElementById("roomName");
 let room = document.getElementById("roomName").value;
-document.getElementById("findRoomButton").addEventListener("click", getRooms);
+document.getElementById("findRoomButton").addEventListener("click", handleFindRoomsButton);
 const roomsError_div = document.getElementById("roomsErrorDiv");
+const roomsInfo_table = document.getElementById("roomsTable");
 
 function handleFindCoursesButton() {
 	console.log("Find courses clicked!");
 	clearFields();
 	getCourses(departmentCode_textInput.value.toLowerCase())
 		.then((response) => response.json())
-		.then((data)=> {
+		.then((data) => {
 			console.log(data);
 			if ("error" in data) {
 				showError(data.error);
@@ -30,8 +31,8 @@ function handleFindCoursesButton() {
 				console.error(data);
 			}
 		}).catch((error) => {
-			showError("Server connection error.", coursesError_div);
-		});
+		showError("Server connection error.", coursesError_div);
+	});
 }
 
 function showCourseTable(data, table) {
@@ -106,13 +107,49 @@ function getCourses(departmentCode) {
 }
 
 function handleFindRoomsButton() {
+	console.log("Find Rooms clicked!");
+	clearFields();
+	getRooms(roomName_textInput.value.toUpperCase())
+		.then((response) => response.json())
+		.then((data) => {
+			console.log(data);
+			if ("error" in data) {
+				showError(data.error, roomsError_div);
+			} else if ("result" in data && Array.isArray(data.result)) {
+				if (!data.result.length) {
+					showError("No room exist for the specified Room Name.", roomsError_div);
+				} else {
+					showRoomTable(data.result, roomsInfo_table);
+				}
+			} else {
+				console.error(data);
+			}
+		}).catch((error) => {
+		showError("Server connection error.", roomsError_div);
+	});
+}
 
+function showRoomTable(data, table) {
+	let thead = table.createTHead();
+	let row = thead.insertRow();
+	for (let field of ["Full name", "Address", "Number of seats", "Type of room", "Furniture"]) {
+		console.log(field);
+		let th = document.createElement("th");
+		let text = document.createTextNode(field);
+		th.appendChild(text);
+		row.appendChild(th);
+	}
+	for (let course of data) {
+		let row = table.insertRow();
+		for (let field in course) {
+			let cell = row.insertCell();
+			let text = document.createTextNode(course[field]);
+			cell.appendChild(text);
+		}
+	}
 }
 
 function getRooms(roomName) {
-	let xhr = new XMLHttpRequest();
-	xhr.open("POST", "./query");
-	xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
 	let json = {
 		"WHERE": {
 			"IS": {
@@ -129,8 +166,13 @@ function getRooms(roomName) {
 			]
 		}
 	}
-	xhr.send(JSON.stringify(json));
-	return xhr.responseText;
+	return fetch('./query', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(json),
+	});
 }
 
 function showError(errorText, errorDiv) {
@@ -144,4 +186,6 @@ function showError(errorText, errorDiv) {
 function clearFields() {
 	coursesError_div.innerText = "";
 	coursesInfo_table.innerHTML = null;
+	roomsError_div.innerText = "";
+	roomsInfo_table.innerHTML = null;
 }
